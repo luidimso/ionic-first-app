bbb6b512455b69c8e7c5b36dd8d8af90b153b2cc
luidimso <luidi_mso@outlook.com>
2018-12-10 15:24:35 -0200

    Criação do menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  pages: [{
+    title: string,
+    component: any
+  }];
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.pages = [{
+      title: 'Home Page',
+      component: HomePage
+    },{
+      title: 'About Page',
+      component: AboutPage
+    }];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +34,8 @@ export class MyApp {
       splashScreen.hide();
     });
   }
-}
 
+  openPage(page: {title: string, component: any}): void{
+    this.nav.setRoot(page.component);
+  }
+}
